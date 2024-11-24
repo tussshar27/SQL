420ff1b46492f7dd638c59310f237872da8f9ec0
Tushar Annam <tushar.annam@gmail.com>
2024-11-24 13:42:28 +0530

    Update DB_Home.sql

--- DB_Home.sql
+++ DB_Home.sql
@@ -167,6 +167,10 @@ from orders group by region order by count desc;
 --it is used in large dataset to know the approx count and not the exact count. 
 -- select count(distinct column) from table; --gives the exact count
 select count(*), count(order_id), count(distinct order_id) from orders;--we can't pass more than one column inside count function, but it can achieved by using subquery.
+SELECT column1, 
+       (SELECT subquery_column FROM subquery_table WHERE condition) AS computed_column
+FROM main_table;
+
 select count(*), count(order_id), approx_count_distinct(order_id) from orders;--approx_count_distinct() function is more efficient than count(distinct column) function.
 SELECT region FROM orders group by region;  -- group by can be worked as distinct keyword.
 
