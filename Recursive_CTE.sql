530751e30acc07e26981a945513165789a35b3c4
Tushar Annam <tushar.annam@gmail.com>
2026-02-17 07:33:04 +0530

    Document recursive CTE syntax and use cases
    Added explanation and example for recursive CTE usage.

--- Recursive_CTE.sql
+++ Recursive_CTE.sql
@@ -51,6 +51,43 @@ to find: We want to find rows where the same value appears in consecutive rows
 
 
 
+-------------------------------------------------------------------------
+There are three parts in recursive CTE:
+1. base query
+2. recursive query
+3. termination check
+
+
+Syntax:
+with RECURSIVE cte_name as (
+    base/anchor query                  -- non recursive term
+    union all
+    recursive query                    -- recursive term
+)
+select * from cte_name
+;
+
+example:
+with RECURSIVE A1 as (
+    select 1 as n
+    
+    union all
+    
+    select n + 1 from A1
+    where n < 3  
+)
+select * from A1
+;
+
+#output:
+1
+2
+3
+
+USE CASE:
+1. count of numbers
+2. finding hierarchical level of all employees
+3. finding routes between cities
 
 
 
